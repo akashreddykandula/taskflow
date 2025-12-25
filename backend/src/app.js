e921982dce2f20e560bcf3ebd45bc08391ea1d34
akashreddykandula <akashreddykandula@gmail.com>
2025-12-25 10:15:19 +0530

    Fix SPA routing for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,10 @@ const app = express ();
 
 app.use (
   cors ({
-    origin: ['http://localhost:5173', 'https://taskflow.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://taskflow-532mhxl7m-akash-kandulas-projects.vercel.app/',
+    ],
     credentials: true,
   })
 );
