7405249c51f1bf004e0ea513c28627cd6ddaa7ab
akashreddykandula <akashreddykandula@gmail.com>
2025-12-25 13:27:21 +0530

    Fix CORS for Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,17 @@
 const express = require ('express');
 const cors = require ('cors');
-const dotenv = require ('dotenv');
-const connectDB = require ('./config/db');
-
-dotenv.config ();
-connectDB ();
 
 const app = express ();
 
 app.use (
   cors ({
-    origin: [
-      'http://localhost:5173',
-      'https://taskflow-532mhxl7m-akash-kandulas-projects.vercel.app/',
-    ],
+    origin: ['http://localhost:5173', 'https://taskflow-two-gules.vercel.app'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
-app.use (express.json ()); // 🔥 REQUIRED
+app.options ('*', cors ());
 
-const authRoutes = require ('./routes/authRoutes');
-app.use ('/api/auth', authRoutes);
-
-const taskRoutes = require ('./routes/taskRoutes');
-
-app.use ('/api/tasks', taskRoutes);
-
-app.get ('/', (req, res) => {
-  res.send ('TaskFlow API is running...');
-});
-
-module.exports = app;
+app.use (express.json ());
